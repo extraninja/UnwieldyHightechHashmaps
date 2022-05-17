3560f0ed4971e49314f3654b10e231f8b546c062
extraninja <104318378+extraninja@users.noreply.github.com>
2022-05-17 21:54:50 +0000

    formatted

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 def computepay(hrs, rate):
-  if hrs <= 40:
-     pay = hrs * rate
-  else:
-     pay = 40 * rate + (hrs - 40) * rate * 1.5
-  return pay
-  
+    if hrs <= 40:
+        pay = hrs * rate
+    else:
+        pay = 40 * rate + (hrs - 40) * rate * 1.5
+    return pay
+
+
 check = True
-while(check == True):
-  try:
-    hrs = input('Enter hours: ')
-    hrs = float(hrs)
-    rate = input('Enter pay rate: ')
-    rate = float(rate)
-    
-    pay = computepay(hrs, rate)
-    y = 'According to computepay, you have earned: $'+ str(pay)
-    print(y) 
-    check = False
-  except:
-    print('Error, please enter numeric input \n')
-    check = True
+while (check == True):
+    try:
+        hrs = input('Enter hours: ')
+        hrs = float(hrs)
+        rate = input('Enter pay rate: ')
+        rate = float(rate)
+
+        pay = computepay(hrs, rate)
+        y = 'According to computepay, you have earned: $' + str(pay)
+        print(y)
+        check = False
+    except:
+        print('Error, please enter numeric input \n')
+        check = True
