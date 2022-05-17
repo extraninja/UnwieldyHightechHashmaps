8791a37f8993599652e3eda79dcd30964817e93d
extraninja <104318378+extraninja@users.noreply.github.com>
2022-05-17 20:36:43 +0000

    fixed issue where numeric input error wasnt showing up right away

--- df.py
+++ df.py
@@ -11,3 +11,23 @@ def computepay(hrs, rate):
     return print(y)                            
   except:
     print('Error, please enter numeric input')
+
+hrs = input('hrs')
+rate= input('rate')
+computepay(hrs, rate)
+
+
+#name=input('what\'s your name? ')
+#print('Hello', name)
+
+#inp=input('Europe floor number? ')
+#usf=int(inp)+1
+#print('USA floor number = ', usf)
+
+#hrs = input('Enter hours: ')
+#rate = input('Enter pay rate: ')
+#if float(hrs) <= 40:
+#  pay = float(hrs) * float(rate)
+#else : 
+#  pay = 40 * float(rate) + (float(hrs) - 40) * float(rate) * 1.5
+#print('You have earned: ', pay)
--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-#name=input('what\'s your name? ')
-#print('Hello', name)
-
-#inp=input('Europe floor number? ')
-#usf=int(inp)+1
-#print('USA floor number = ', usf)
-
-#hrs = input('Enter hours: ')
-#rate = input('Enter pay rate: ')
-#if float(hrs) <= 40:
-#  pay = float(hrs) * float(rate)
-#else : 
-#  pay = 40 * float(rate) + (float(hrs) - 40) * float(rate) * 1.5
-#print('You have earned: ', pay)
-
-
-
-
 def computepay(hrs, rate):
   if hrs <= 40:
      pay = hrs * rate
@@ -25,14 +7,14 @@ def computepay(hrs, rate):
   
 try:
   hrs = input('Enter hours: ')
-  rate = input('Enter pay rate: ')
   hrs = float(hrs)
+  rate = input('Enter pay rate: ')
   rate = float(rate)
   
   pay = computepay(hrs, rate)
   y = 'According to computepay, you have earned: $'+ str(pay)
-  print(y)
-                                
+  print(y) 
+
 except:
   print('Error, please enter numeric input')
   
