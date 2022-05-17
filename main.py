df4b95419b197a8ea158f8258cb6bf11fa2e1329
extraninja <104318378+extraninja@users.noreply.github.com>
2022-05-17 21:08:39 +0000

    added while loop upon error

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ def computepay(hrs, rate):
      pay = 40 * rate + (hrs - 40) * rate * 1.5
   return pay
   
-try:
-  hrs = input('Enter hours: ')
-  hrs = float(hrs)
-  rate = input('Enter pay rate: ')
-  rate = float(rate)
-  
-  pay = computepay(hrs, rate)
-  y = 'According to computepay, you have earned: $'+ str(pay)
-  print(y) 
-
-except:
-  print('Error, please enter numeric input')
-  
+check = True
+while(check == True):
+  try:
+    hrs = input('Enter hours: ')
+    hrs = float(hrs)
+    rate = input('Enter pay rate: ')
+    rate = float(rate)
+    
+    pay = computepay(hrs, rate)
+    y = 'According to computepay, you have earned: $'+ str(pay)
+    print(y) 
+    check = False
+  except:
+    print('Error, please enter numeric input \n')
+    check = True
